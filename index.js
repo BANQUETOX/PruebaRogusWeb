2ba942c336c80c4cf8526a60905ac19efca937d8
Banquetox <banquetox@gmail.com>
2024-06-18 09:47:54 -0600

    Form validation js and html, visual format, request to APIs,

--- index.js
+++ index.js
@@ -1,33 +1,65 @@
 let searchId = document.getElementById("search-id")
+let resultSection = document.getElementById("result")
+let errorMessage = document.getElementById("result-error")
+let name = document.getElementById("name")
+let lastName = document.getElementById("last-name")
+let secondLastName = document.getElementById("last-name-second")
+let birthday = document.getElementById("birthday")
+let searchUrl
+
+
+searchId.addEventListener('input', function(e) {
+    const input = e.target;
+    let value = input.value.replace(/\D/g, ''); 
+    if (value.length > 2) {
+        value = value.substring(0, 2) + '-' + value.substring(2);
+    }
+    if (value.length > 7) {
+        value = value.substring(0, 7) + '-' + value.substring(7, 11);
+    }
+    input.value = value;
+});
 
 
 $("#search-button-linq").click(()=>{
-    $.ajax({
-        url: 'https://rogusapipractice.azurewebsites.net/api/Person/GetPersonDataLinq?id=' + searchId.value,
-        method: 'GET',
-        success:(data)=>{
-            console.log(data)
-            
-        },
-        error: (error)=>{
-            console.error(error)
-        } 
-        
-    })
+    searchUrl = 'https://rogusapipractice.azurewebsites.net/api/Person/GetPersonDataLinq?id='
 
 })
 
 $("#search-button-sqlCommnad").click(()=>{
-    $.ajax({
-        url: 'https://rogusapipractice.azurewebsites.net/api/Person/GetPersonDataSqlCommand?id=' + searchId.value,
-        method: 'GET',
-        success: function (data) {
-            console.log(data)
-        },
-        error: (error) =>{
-            console.error(error)
-        } 
-        
-    })
+    searchUrl = 'https://rogusapipractice.azurewebsites.net/api/Person/GetPersonDataSqlCommand?id='
 })
+
+
+
+$("#form").submit((e)=>{
+    let rawId = searchId.value.replace(/-/g, '')
+    $.ajax({
+                url: searchUrl + rawId,
+                method: 'GET',
+                success:(data)=>{
+                    resultSection.classList.remove('d-none')
+                    errorMessage.classList.add('d-none')
+                    showPerson(data)
+                    
+                },
+                error: (error)=>{
+                    resultSection.classList.add('d-none')
+                    errorMessage.classList.remove('d-none')
+                    console.log(error)
+                } 
+                
+            })
+
+   
+    e.preventDefault()
+})
+
+function showPerson(data){
+    name.innerHTML = "Nombre: " + data.nombre
+    lastName.innerHTML = "Apellido: " + data.apellido1
+    secondLastName.innerHTML = "Segundo Apellido: " + data.apellido2
+    birthday.innerHTML = "Fecha de nacimiento: " + data.fechaNacimiento
+}
+
 
